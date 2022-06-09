252f7c9e34d9a54f007d9461e07ca774b5727077
Lapin Andrey <nethack@mail.ru>
2022-06-09 13:38:05 +0400

    refactor on native cfetch

--- src/app/fetchAPI.ts
+++ src/app/fetchAPI.ts
@@ -1,6 +1,13 @@
+//@ts-ignore
 import axios from 'axios'
 
+// export const fetchAPI = async (url:string):Promise<string> => {
+//     const { data } = await axios.get(url);
+//     return data.files['view.json'].content
+// }
+
 export const fetchAPI = async (url:string):Promise<string> => {
-    const { data } = await axios.get(url);
+    
+    const data = await fetch(url).then(data => data.json())
     return data.files['view.json'].content
 }
